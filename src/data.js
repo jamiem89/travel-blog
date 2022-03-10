75a615a44113ae006a5cf453f4b7628ef8cb4a7e
Jamie Moore <jamie@rabbithole.co.uk>
2022-03-10 12:48:47 +0000

    added github pages deploy scripts

--- src/data.js
+++ src/data.js
@@ -10,12 +10,21 @@ export default [
         imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
     },
     {
-        title: "Mount Fuji 2",
-        location: "Japan 2",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2022",
-        endDate: "24 Jan, 2022",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists. 2",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas2"
+        title: "Lake Bled",
+        location: "Slovenia",
+        googleMapsUrl: "https://www.google.com/maps/place/Lake+Bled/@46.3639194,14.0908835,16z/data=!3m1!4b1!4m5!3m4!1s0x477a9139b9d9e421:0x83cd8331dd0107ac!8m2!3d46.363598!4d14.0938053",
+        startDate: "Mar, 2019",
+        endDate: "Apr, 2019",
+        description: "Lake Bled is a lake in the Julian Alps of the Upper Carniolan region of northwestern Slovenia, where it adjoins the town of Bled.",
+        imageUrl: "https://source.unsplash.com/tqwDMQrODHs"
+    },
+    {
+        title: "Riga",
+        location: "Latvia",
+        googleMapsUrl: "https://www.google.com/maps/place/Riga,+Latvia/@56.9715833,23.9890808,11z/data=!3m1!4b1!4m5!3m4!1s0x46eecfb0e5073ded:0x400cfcd68f2fe30!8m2!3d56.9496487!4d24.1051865",
+        startDate: "Dec, 2020",
+        endDate: "Jan, 2021",
+        description: "Riga, Latvia’s capital, is set on the Baltic Sea at the mouth of the River Daugava. It's considered a cultural center and is home to many museums and concert halls. ",
+        imageUrl: "https://source.unsplash.com/jFqUJacYIsw"
     }
 ]
